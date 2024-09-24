7c69edf20afba73e979e06d16944d727407bd6bd
nguyensang <nguyensang@vnvn.net>
2024-09-24 11:53:10 +0700

    fix code

--- src/actions/seed.ts
+++ src/actions/seed.ts
@@ -5,20 +5,20 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 export async function InitAdmin() {
-    try {
-        const result = await prisma.user.create({
-            data: {
-              email: 'nvsang2670@gmail.com',
-              fullName: 'Nguyễn Văn Sang',
-              username: 'diepminh2670',
-              phone: '0386237067',
-              password: bcrypt.hashSync('Sang2607200209@', 10),
-              role: 'ADMIN',
-            },
-          });
+  try {
+    const result = await prisma.user.create({
+      data: {
+        email: "nvsang2670@gmail.com",
+        fullName: "Nguyễn Văn Sang",
+        username: "diepminh2670",
+        phone: "0386237067",
+        password: bcrypt.hashSync("Sang2607200209@", 10),
+        role: "ADMIN",
+      },
+    });
 
-        return result
-    } catch (error) {
-        console.log(error)
-    }
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
