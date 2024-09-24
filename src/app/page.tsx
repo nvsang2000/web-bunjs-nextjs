37f93b359aedc08f8f83d1af4720670f380b9ebb
nguyensang <nguyensang@vnvn.net>
2024-09-24 13:27:03 +0700

    fix code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@
 import React, { useEffect } from "react";
 import Link from "next/link";
 import { InitAdmin } from "../actions/seed";
-import { Button } from "antd";
+import { Button, message } from "antd";
 
 export default async function Home() {
+
+  const generateInit = async () => {
+    const result = await InitAdmin()
+    message.success(`${result}`)
+  }
 
   return (
     <div className="p-[40px]">
@@ -17,6 +22,7 @@ export default async function Home() {
           </Link>
         </span>
       </div>
+      <div className="mt-[20px]"><Button onClick={generateInit}>Generate addmin</Button></div>
     </div>
   );
 }
